c3a147177b5099c9b356535efd8768057f706551
Kevyn Oliveira <kevyn.oliveira@maiscodeopt3.local>
2020-03-14 13:45:19 -0400

    Deixando o código mais enxuto

--- backend/controllers/PokemonController.js
+++ backend/controllers/PokemonController.js
@@ -2,25 +2,23 @@ const Pokemon = require('../models/Pokemon');
 
 module.exports = {
 
+    // Controller resposável pela visualização de todos os pokemons
+
     async index (req, res) {
         const pokemons = await Pokemon.find();
 
         return res.json(pokemons);
     },
 
+    // Controller resposável pelo cadastramento de novos pokemons
+
     async store (req, res) {
 
-        const { row, name } = req.body;
-
-        let pokemon = await Pokemon.findOne({ name });
-
-        if (!pokemon) {
-            pokemon =  await Pokemon.create({
-                row,
-                name
-            }); 
-        }
+        const poke = req.body;
+        
+        const pokemon = await Pokemon.create(poke);
 
         return res.json(pokemon);
+        
     }
-}
+}   
