5de17f9c4c72a384414a62dc606c663c39298ca9
Kevyn Oliveira <kevyn.oliveira@maiscodeopt3.local>
2020-03-13 23:22:33 -0400

    Configuração da conexão

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Conexão com mongoDB, através de um Cluster 
+
+mongoose.connect('mongodb+srv://redfox:redfox@cluster0-pnzvf.mongodb.net/pokemons?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Middleware que analisa as requisições en Json
+
+app.use(express.json());
+app.use(routes);
+
+// Porta na qual a aplicação podera ser acessada
+
+app.listen(3333);
