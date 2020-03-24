ea7673f0f63d4710004966833901403141d6b899
oseias <oseiasal@gmail.com>
2020-03-24 11:49:04 -0300

    a barra de navegação foi adicionada

---
+++ src/components/PokeBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default function PokeBar() {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Navbar.Brand>PokeDex</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#">FindByType</Nav.Link>
+                        <Nav.Link href="#">FindByWeather</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
