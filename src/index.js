cef529f670321389000335f271dbe09fa1f713c9
oseias <oseiasal@gmail.com>
2020-03-24 19:51:51 -0300

    adicinado css do bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
