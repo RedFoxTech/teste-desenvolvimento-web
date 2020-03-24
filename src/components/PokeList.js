1840e5891e34cbc2c2e3a142f7a320df1cff8e26
oseias <oseiasal@gmail.com>
2020-03-24 19:54:26 -0300

    adicionado renderização após o fetch

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Col, Container, Row, Card } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import PokeCard from './PokeCard'
 
-export default function PokeList({ typeOne, ATK, DFS, SFX }) {
+export default function PokeList({ data }) {
+
     return (
         <Container className="bg-warning" fluid="xs" style={{ height: '90vh' }}>
             <Row className="p-2 m-0">
-                <Col>
-                    <PokeCard />
-                </Col>
+                <Row className="p-5 flex-row justify-content-center" lg={5}>
+
+                    {data.length == 0 ? "Nada a exibir" : data[0].map((item, index) => {
+                        return <PokeCard item={item} />
+                    })}
+
+                </Row>
             </Row>
         </Container>
     )
