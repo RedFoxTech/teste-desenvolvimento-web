95b8f21f85769781b8d7dc735fd074998d463f06
oseias <oseiasal@gmail.com>
2020-03-24 19:53:07 -0300

    adicionado useState e handler do API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { getAllPokes } from './components/API';
 import PokeBar from './components/PokeBar';
 import PokeList from './components/PokeList';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  let all = [];
+
+  function getAll() {
+    console.log("foi");
+    getAllPokes().then(pokes => {
+      all = [pokes]
+      setPokemons(all)
+    }).catch(e => alert(e))
+  }
+
   return (
-<>
-    <PokeBar />
-    <PokeList />
-</>
+    <>
+      <PokeBar getAll={getAll} />
+      <PokeList data={pokemons} />
+    </>
   );
 }
 
