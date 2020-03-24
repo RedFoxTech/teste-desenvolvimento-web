ddd7c0292edfe441f985e995a112490f61765245
oseias <oseiasal@gmail.com>
2020-03-24 19:55:18 -0300

    adicionado função como props

--- src/components/PokeBar.js
+++ src/components/PokeBar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
 
-export default function PokeBar() {
+
+export default function PokeBar({getAll}) {
+
     return (
         <>
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Navbar.Brand>PokeDex</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#">FindByType</Nav.Link>
+                        <Nav.Link href="#" onClick={(e) => {
+                             getAll()
+                        }} >FindByType</Nav.Link>
                         <Nav.Link href="#">FindByWeather</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
