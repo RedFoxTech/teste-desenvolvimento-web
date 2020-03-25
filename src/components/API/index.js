35d5aeada80774ce386efb0193deea946a9cbe97
oseias <oseiasal@gmail.com>
2020-03-25 15:01:26 -0300

    adicionada função para filtrar por nome e por tipo

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -1,4 +1,14 @@
 export const getAllPokes = () => fetch('http://localhost/getall')
     .then(response => response.json())
     .then((dados) => dados)
-    .catch((e) => alert(e))
+    .catch((e) => console.log(e));
+
+export const filterPokesByTypeOne = (typeOne) => fetch(`http://localhost/${typeOne}`)
+    .then(response => response.json())
+    .then((dados) => dados)
+    .catch((e) => console.log(e));
+    
+export const filterByName = (Name) => fetch(`http://localhost/pokemon/${Name}`)
+    .then(response => response.json())
+    .then((dados) => dados)
+    .catch((e) => console.log(e));
