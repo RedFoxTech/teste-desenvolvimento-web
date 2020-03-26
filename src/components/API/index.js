1a5401085b0be41ad0635585ebc98b1cf1efd2b5
oseias <oseiasal@gmail.com>
2020-03-26 11:23:33 -0300

    endereço da api atualizado para 10.128.0.2

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -1,15 +1,15 @@
 
-export const getAllPokes = () => fetch('http://localhost:8088/getall')
+export const getAllPokes = () => fetch('http://10.128.0.2:8088/getall')
     .then(response => response.json())
     .then((dados) => dados)
     .catch((e) => console.log(e));
 
-export const filterPokesByTypeOne = (typeOne) => fetch(`http://localhost:8088/${typeOne}`)
+export const filterPokesByTypeOne = (typeOne) => fetch(`http://10.128.0.2:8088/${typeOne}`)
     .then(response => response.json())
     .then((dados) => dados)
     .catch((e) => console.log(e));
     
-export const filterByName = (Name) => fetch(`http://localhost:8088/pokemon/${Name}`)
+export const filterByName = (Name) => fetch(`http://10.128.0.2:8088/pokemon/${Name}`)
     .then(response => response.json())
     .then((dados) => dados)
     .catch((e) => console.log(e));
