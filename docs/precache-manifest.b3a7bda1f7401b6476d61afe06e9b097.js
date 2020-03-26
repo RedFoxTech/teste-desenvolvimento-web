7b2566f5eb84d7be9bf939bbed67ef5657f73da0
oseias <oseiasal@gmail.com>
2020-03-26 12:39:08 -0300

    testando o github pages
    houveram alguns problemas no servidor do GCP devido e o apache

---
+++ docs/precache-manifest.b3a7bda1f7401b6476d61afe06e9b097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8b5a72f30fe00582bfbe7f3e1baebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "696cb7fd619685d17230",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "696cb7fd619685d17230",
+    "url": "/static/js/2.df48196d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.df48196d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2198339d005d93d1173e",
+    "url": "/static/js/main.9544132d.chunk.js"
+  },
+  {
+    "revision": "a9cbcdd849391898e594",
+    "url": "/static/js/runtime-main.17461257.js"
+  }
+]);
