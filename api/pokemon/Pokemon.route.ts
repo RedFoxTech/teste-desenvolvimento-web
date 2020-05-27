bbcb06e1a00348c056bfd57f706b0f383d6b4bd8
Alan Alves <alan.demit@hotmail.com>
2020-05-27 15:32:03 -0300

    Adding multer middware for pokemon route

--- api/pokemon/Pokemon.route.ts
+++ api/pokemon/Pokemon.route.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import PokemonController from './Pokemon.controller';
+import multer from 'multer'
+const uploadMiddleware = multer({ dest: 'uploads' });
 const controller = new PokemonController();
 const routes = Router();
 
 routes.post('/', controller.create);
-routes.post('/upload', controller.upload);
+routes.post('/upload', uploadMiddleware.single('file'), controller.upload);
 routes.get('/', controller.list);
 routes.get('/:_id', controller.listOne);
 routes.put('/:_id', controller.update);
 routes.delete('/:_id', controller.remove);
+
+export default routes;
