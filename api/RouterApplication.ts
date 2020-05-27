a55cb538b989184806b21227e0eda9c62148a48c
Alan Alves <alan.demit@hotmail.com>
2020-05-27 15:31:02 -0300

    Adding Pokemons routes to main app

--- api/RouterApplication.ts
+++ api/RouterApplication.ts
@@ -2,6 +2,8 @@ import Express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import dbConnector from './config/DatabaseConnector';
 import envBuilder from './config/EnvBuilder';
+import pokemonRoutes from './pokemon/Pokemon.route';
+import bodyParser from 'body-parser';
 
 export default class RouterApplication {
     public app: Express.Application;
@@ -21,7 +23,10 @@ export default class RouterApplication {
     }
 
     private configRoutes() {
-        this.app.use(Express.json());
-        this.app.use(morgan('dev'))
+        // this.app.use(Express.json());
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(morgan('dev'));
+        this.app.use('/pokemons', pokemonRoutes);
     }
 }
