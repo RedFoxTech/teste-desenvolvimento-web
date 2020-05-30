039fd06c8dde5302dd6a1480ebd7769dea61e271
luiz-ortega <luizngust@hotmail.com>
2020-05-29 21:13:21 -0300

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import routes from'./routes'
 
 import './database'
@@ -11,6 +12,7 @@ class App {
   }
 
   middlewares () {
+    this.server.use(cors())
     this.server.use(express.json())
   }
 
