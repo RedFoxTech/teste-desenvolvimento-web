727ed2898c000597a0c1bddb284f83e77abc7987
luiz-ortega <luizngust@hotmail.com>
2020-05-31 17:50:09 -0300

    screens and readme

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://157.245.95.117:3333/',
+    // baseURL: 'http://157.245.95.117:3333/',
+    baseURL: 'http://localhost:3333/',
 });
 
 export default api;
