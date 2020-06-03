442d5e297bc605ed930f959a5d193db2e2318d25
Pedro Borges <ppmbjr@gmail.com>
2020-06-03 08:06:49 -0300

    added eslint, prettier and some dependencies to the project

---
+++ web/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+    plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    "react", "jsx-a11y", "import", "react-hooks", "prettier"
+  ],
+  rules: {
+    "prettier/prettier": ["error", {
+     "endOfLine":"auto"
+   }],
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react/jsx-one-expression-per-line": "off",
+    "global-require": "off",
+    "react-native/no-raw-text": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      }
+    }
+  }
+}
--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
