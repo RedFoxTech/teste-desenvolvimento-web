f7a6a8fa4705a2596e11833d45ccb1339789d6ee
Pedro Borges <ppmbjr@gmail.com>
2020-06-04 07:34:01 -0300

    chore: added new database file

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -3,7 +3,7 @@ import path from 'path';
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+        filename: path.resolve(__dirname, 'src', 'database', 'data.sqlite'),
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const connection = knex({
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, 'database.sqlite'),
+        filename: path.resolve(__dirname, 'data.sqlite'),
     },
     useNullAsDefault: true,
 });
