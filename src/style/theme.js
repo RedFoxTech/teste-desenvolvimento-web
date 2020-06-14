38771df0938b3c3da22941045b3d793967268eba
vitor-slopes <vitor_slopes@hotmail.com>
2020-06-13 23:36:25 -0300

    preparing environment

---
+++ src/style/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e86e5a',
+    },
+    secondary: {
+      main: '#b8b8b8',
+    }
+  }
+});
