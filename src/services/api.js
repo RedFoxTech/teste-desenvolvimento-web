b997e4916f2c5de0fe495ee5535f484f5b7ef8f7
KennedyOn <kennedy2000silva@hotmail.com>
2020-07-16 13:25:11 -0300

    adicionei variaveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
 	baseURL:
 		process.env.APP_TYPE !== 'development'
-			? 'https://pokedex-backend-k.herokuapp.com/'
+			? process.env.API_URL
 			: 'http://localhost:3333/',
 });
 
