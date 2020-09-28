444ca10c0a5472b71aa95aad099d8fcfb82f97a8
Thulio <thulio.alves@gmail.com>
2020-09-28 13:55:27 -0300

    Construindo Rotas do App para a página de artibutos dos pokemons

--- pokedexgo/src/App.js
+++ pokedexgo/src/App.js
@@ -1,18 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import fundo from './assets/fundo.png';
 import NavBar from './components/layout/NavBar';
 import PainelPokemon from './components/layout/PainelPokemon';
+import Pokemon from './components/pokemon/Pokemon';
 
 function App() {
   return (
-    <div className="App" style={{ background: `url(${fundo})` }}>
-      <NavBar />
-      <div className="container">
-        <PainelPokemon />
+    <Router>
+      <div className="App" style={{ background: `url(${fundo})` }}>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={PainelPokemon} />
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
