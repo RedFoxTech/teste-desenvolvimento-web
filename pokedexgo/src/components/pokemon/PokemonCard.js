104159ad98e379c57ffade0cd84363688966b065
Thulio <thulio.alves@gmail.com>
2020-09-28 05:57:47 -0300

    Fazendo com que a página não seja selecionada

--- pokedexgo/src/components/pokemon/PokemonCard.js
+++ pokedexgo/src/components/pokemon/PokemonCard.js
@@ -14,6 +14,10 @@ const Card = styled.div`
   &:hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   }
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+  -o-user-select: none;
 `;
 
 export default class PokemonCard extends Component {
