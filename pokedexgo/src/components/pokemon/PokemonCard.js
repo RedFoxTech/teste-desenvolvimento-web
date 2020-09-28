87e8f083d8e3f41167e831046bfad28907a086e3
Thulio <thulio.alves@gmail.com>
2020-09-28 04:53:00 -0300

    Carregando imagens dos pokemons nos cards

--- pokedexgo/src/components/pokemon/PokemonCard.js
+++ pokedexgo/src/components/pokemon/PokemonCard.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+`;
 
 export default class PokemonCard extends Component {
 
@@ -6,12 +12,14 @@ export default class PokemonCard extends Component {
     name: '',
     imgUrl: '',
     pokemonIndex: '',
+    carregandoImg: true,
+    muitasRequests: false,
   }
 
   componentDidMount() {
     const { name, url } = this.props;
     const pokemonIndex = url.split('/')[url.split('/').length - 2];
-    const imgUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon${pokemonIndex}.png?raw=true`
+    const imgUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
 
     this.setState({
       name,
@@ -23,12 +31,19 @@ export default class PokemonCard extends Component {
   render() {
 
     return (
-      <div className="col-md-3 col-md-6 mb-5">
+      <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
 
           <h5 className="card-header">
             {this.state.pokemonIndex}
           </h5>
+
+          <Sprite
+            className="card-img-top rounded mx-auto mt-2"
+            onLoad={() => this.setState({ carregandoImg: false })}
+            onError={() => this.setState({ muitasRequests: true })}
+            src={this.state.imgUrl}
+          />
 
           <div className="card-body mx-auto">
             <h6 className="card-title">
