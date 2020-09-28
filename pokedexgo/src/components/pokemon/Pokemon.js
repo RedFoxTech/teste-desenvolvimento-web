c4784e94f6c3709c49d6a5ddcfdc05dbabea60eb
Thulio <thulio.alves@gmail.com>
2020-09-28 14:37:27 -0300

    Mapeando os stats básicos do pokemon

--- pokedexgo/src/components/pokemon/Pokemon.js
+++ pokedexgo/src/components/pokemon/Pokemon.js
@@ -40,6 +40,29 @@ export default class Pokemon extends Component {
     const imgUrl = pokemonInfo.data.sprites.front_default;
 
     let { hp, attack, defense, speed, specialAttack, specialDefense } = '';
+
+    pokemonInfo.data.stats.map(stat => {
+      switch (stat.stat.name) {
+        case 'hp':
+          hp = stat['base_stat'];
+          break;
+        case 'attack':
+          attack = stat['base_stat'];
+          break;
+        case 'defense':
+          defense = stat['base_stat'];
+          break;
+        case 'speed':
+          speed = stat['base_stat'];
+          break;
+        case 'special-attack':
+          specialAttack = stat['base_stat'];
+          break;
+        case 'special-defense':
+          specialDefense = stat['base_stat'];
+          break;
+      }
+    });
   }
 
   render() {
