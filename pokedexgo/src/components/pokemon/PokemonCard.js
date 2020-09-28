3ebd7229ea8a98f2f7f880d54c00ffaf6cad0097
Thulio <thulio.alves@gmail.com>
2020-09-28 05:33:53 -0300

    Adicionando estilo para os card, box shadow e transition com hover

--- pokedexgo/src/components/pokemon/PokemonCard.js
+++ pokedexgo/src/components/pokemon/PokemonCard.js
@@ -8,6 +8,14 @@ const Sprite = styled.img`
   display: none;
 `;
 
+const Card = styled.div`
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+`;
+
 export default class PokemonCard extends Component {
 
   state = {
@@ -34,7 +42,7 @@ export default class PokemonCard extends Component {
 
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div className="card">
+        <Card className="card">
 
           <h5 className="card-header">
             {this.state.pokemonIndex}
@@ -77,7 +85,7 @@ export default class PokemonCard extends Component {
             </h6>
           </div>
 
-        </div>
+        </Card>
       </div>
     );
 
