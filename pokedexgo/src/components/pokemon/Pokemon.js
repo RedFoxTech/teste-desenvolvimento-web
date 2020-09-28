14c4b997f3b7dcccac24c4510b065bf3c1fcf875
Thulio <thulio.alves@gmail.com>
2020-09-28 14:19:34 -0300

    Carregando as informações dos pokemons na página

--- pokedexgo/src/components/pokemon/Pokemon.js
+++ pokedexgo/src/components/pokemon/Pokemon.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react';
 import axios from 'axios';
+import React, { Component } from 'react';
 
 export default class Pokemon extends Component {
+  state = {
+    name: '',
+    pokemonIndex: '',
+    imgUrl: '',
+  };
+
+  async componentDidMount() {
+    const { pokemonIndex } = this.props.match.params;
+
+    //Urls das informações dos Pokemons
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+    const especiesPokemonUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+
+    //Solicitando informações dos Pokemons
+    const pokemonInfo = await axios.get(pokemonUrl);
+
+    const name = pokemonInfo.data.name;
+    this.setState({ name });
+  }
+
   render() {
     return (
-      <div />
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
     );
   }
 }
