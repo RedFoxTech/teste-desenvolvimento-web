b8b372eaa83357c0a679542926f32728be934487
Kelvy <kelvy0064@gmail.com>
2020-10-01 17:21:00 -0300

    added multer s3 aws

---
+++ BACKEND/src/config/multer.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const crypto = require('crypto')
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
+require('dotenv').config()
+
+const storageTypes = {
+
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: process.env.BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, cb) => {
+            crypto.randomBytes(16, (err, hash) => {
+                if (err) cb(err)
+                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                cb(null, fileName)
+            })
+        }
+    })
+}
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: storageTypes[process.env.STORAGE_TYPE],
+    limits: {
+        fileSize: 2 * 1024 * 1024
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png',
+            'image/gif'
+        ]
+        if (allowedMimes.includes(file.mimetype)) {
+            cb(null, true)
+        } else {
+            cb(new Error('Invalid file type.'))
+        }
+    }
+}
