3ed924d6fc3b2810ef209286eb45268b69800341
lucas <lucass@pop-os.localdomain>
2020-10-06 18:00:41 -0300

    Create server with Overnight and express

---
+++ api/src/server.ts
@@ -0,0 +1,44 @@
+import './utils/module-alias'
+import { Server } from '@overnightjs/core'
+import express, { Application } from 'express'
+import * as database from '@src/database'
+
+export class SetupServer extends Server {
+  constructor (private port = 3000) {
+    super()
+  }
+
+  public async init (): Promise<void> {
+    this.setupExpress()
+    this.setupControllers()
+    await this.databaseSetup()
+  }
+
+  private setupExpress (): void {
+    this.app.use(express.json())
+  }
+
+  private setupControllers (): void {
+    this.addControllers([
+
+    ])
+  }
+
+  private async databaseSetup (): Promise<void> {
+    await database.connect()
+  }
+
+  public start (): void {
+    this.app.listen(this.port, () => {
+      console.info('Server is running on port: ' + this.port)
+    })
+  }
+
+  public async close (): Promise<void> {
+    await database.close()
+  }
+
+  public getApp (): Application {
+    return this.app
+  }
+}
