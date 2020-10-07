7de0f28ff86691fe31a51769ed64f5140bf09472
lucas <lucass@pop-os.localdomain>
2020-10-06 21:37:49 -0300

    Add error tests for PokemonController

--- api/test/functional/pokemon.test.ts
+++ api/test/functional/pokemon.test.ts
@@ -1,6 +1,10 @@
 import { Pokemon } from '@src/models/pokemon'
 
 describe('Pokemon functional tests', () => {
+  beforeEach(async () => {
+    await Pokemon.deleteMany({})
+  })
+
   it('should return a list of pokemons', async () => {
     const response = await global.testRequest.get('/pokemons')
 
@@ -26,10 +30,45 @@ describe('Pokemon functional tests', () => {
     }
 
     const pokemon = await new Pokemon(defaultPokemon)
-    console.log(pokemon._id)
 
     const response = await global.testRequest.delete(`/pokemons/${pokemon._id}`)
 
     expect(response.status).toBe(200)
+  })
+
+  it('should return a validation error when a fild is missing', async () => {
+    const defaultPokemon = {
+      'Pokedex Number': 1
+    }
+
+    const response = await global.testRequest.post('/pokemons').send(defaultPokemon)
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({
+      message: 'Pokemon validation failed: Name: Path `Name` is required.'
+    })
+  })
+
+  it('should return a validation error when trying to delete a pokemon', async () => {
+    const response = await global.testRequest.delete('/pokemons/invalid-id')
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({
+      message: 'Cast to ObjectId failed for value "invalid-id" at path "_id" for model "Pokemon"'
+    })
+  })
+
+  it('Should return 400 when the Pokemon Name already exists', async () => {
+    const defaultPokemon = {
+      Name: 'Pokemon'
+    }
+    await global.testRequest.post('/pokemons').send(defaultPokemon)
+    const response = await global.testRequest.post('/pokemons').send(defaultPokemon)
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({
+      message:
+        'E11000 duplicate key error collection: pokemon-test.pokemons index: Name_1 dup key: { Name: "Pokemon" }'
+    })
   })
 })
