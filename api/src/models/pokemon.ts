404ac4abc8a54e06bf01f24c28ab5c3bfcffe1f5
lucas <lucass@pop-os.localdomain>
2020-10-06 21:37:01 -0300

    Fix

--- api/src/models/pokemon.ts
+++ api/src/models/pokemon.ts
@@ -2,36 +2,36 @@ import mongoose, { Schema, Document, Model } from 'mongoose'
 
 export interface IPokemon {
   _id?: string;
-  Row: number,
-  Name: string,
-  'Pokedex Number': number,
-  'Img name': string,
-  Generation: number,
-  'Evolution Stage': string,
-  Evolved: number,
-  FamilyID: number,
-  'Cross Gen': number,
-  'Type 1': string,
-  'Type 2': string,
-  'Weather 1': string,
-  'Weather 2': string,
-  'STAT TOTAL': number,
-  ATK: number,
-  DEF: number,
-  STA: number,
-  Legendary: number,
-  Aquireable: number,
-  Spawns: number,
-  Regional: number,
-  Raidable: number,
-  Hatchable: number,
-  Shiny: number,
-  Nest: number,
-  New: number,
-  'Not-Gettable': number,
-  'Future Evolve': number,
-  '100% CP @ 40': number,
-  '100% CP @ 39': number
+  Row: number;
+  Name: string;
+  'Pokedex Number': number;
+  'Img name': string;
+  Generation: number;
+  'Evolution Stage': string;
+  Evolved: number;
+  FamilyID: number;
+  'Cross Gen': number;
+  'Type 1': string;
+  'Type 2': string;
+  'Weather 1': string;
+  'Weather 2': string;
+  'STAT TOTAL': number;
+  ATK: number;
+  DEF: number;
+  STA: number;
+  Legendary: number;
+  Aquireable: number;
+  Spawns: number;
+  Regional: number;
+  Raidable: number;
+  Hatchable: number;
+  Shiny: number;
+  Nest: number;
+  New: number;
+  'Not-Gettable': number;
+  'Future Evolve': number;
+  '100% CP @ 40': number;
+  '100% CP @ 39': number;
 }
 
 interface PokemonModel extends Omit<IPokemon, '_id'>, Document {}
