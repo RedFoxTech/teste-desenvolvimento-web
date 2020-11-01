bb4e4587aea767293df4d3773fd76ae4d0f5b860
AliceAAtalla <aliceatalla36@gmail.com>
2020-11-01 18:28:08 -0300

    Adicionando o cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const { pokemonRouter } = require('./router');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
