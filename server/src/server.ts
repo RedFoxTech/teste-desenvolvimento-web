d6d21719d3b459d7babe2cc192980607594852c5
Lucas Aoun <souolucas2009@gmail.com>
2020-11-05 12:16:25 -0300

    cors add

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.listen(3333)
