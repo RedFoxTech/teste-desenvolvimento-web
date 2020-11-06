eea3f56e2154fe24af2d8d84a10c94132823aa36
Lucas Aoun <souolucas2009@gmail.com>
2020-11-06 13:48:40 -0300

    pokemonAdd add

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Home from './pages/Home'
+import pokemonAdd from './pages/PokemonAdd'
 
 
 function Routes(){
@@ -9,7 +10,8 @@ function Routes(){
         <BrowserRouter>
             <Switch>
             <Route path="/" exact component={Landing}/>
-            <Route path="/home" exact component={Home}/>
+            <Route path="/home" component={Home}/>
+            <Route path="/pokemonAdd" component={pokemonAdd}/>
             </Switch>
         </BrowserRouter>
     )
