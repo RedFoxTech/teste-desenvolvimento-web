640c8858c7ccbc8c819c8717d0d9decf615b796a
Lucas Aoun <souolucas2009@gmail.com>
2020-11-09 12:18:14 -0300

    fix connection

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -5,7 +5,8 @@ const connection = knex({
     client: 'sqlite3',
     connection:{
         filename: path.resolve(__dirname, 'database.sqlite'),
-    }
+    },
+    useNullAsDefault: true  
   });
 
 export default connection
