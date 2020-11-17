79400fa13fe8ab1d43803144088056b61f490bcf
Lucas Aoun <souolucas2009@gmail.com>
2020-11-17 12:59:13 -0300

    upload.array to upload.single

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -11,7 +11,7 @@ const upload = multer(uploadConfig)
 //padrao controller (index, create, show, update, delete)
 routes.get('/pokemon', pokemonController.index)
 routes.get('/pokemon/:pokedexNumber', pokemonController.show)
-routes.post('/pokemon', upload.array('image'), pokemonController.create)
+routes.post('/pokemon', upload.single('image'), pokemonController.create)
 
 
 
