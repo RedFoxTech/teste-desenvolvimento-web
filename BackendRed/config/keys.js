c5c72828e8927356f747372f5d1fb4b42d19e9be
domuserk <rgdesenhos321@gmail.com>
2020-12-13 09:54:36 -0300

    DeletingBackend

--- BackendRed/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://MERN:mern@cluster0.pz9sc.mongodb.net/MERN_db?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-}
--- BackendRed/validation/valid-text.js
+++
@@ -1,5 +0,0 @@
-const validText = str => {
-    return typeof str === 'string' && str.trim().length > 0;
-}
-
-module.exports = validText;
