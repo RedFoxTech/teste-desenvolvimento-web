2ed6e7e722987991e0e71e396d6181e46e751e7d
Your Name <you@example.com>
2021-03-21 19:11:02 -0300

    Implementando método de procurar pokémon.

--- web/src/contexts/pokemonApi.tsx
+++ web/src/contexts/pokemonApi.tsx
@@ -1,67 +1,119 @@
 import axios from 'axios';
 import { createContext, ReactNode, useEffect, useState } from 'react';
 
-interface IPokemonApiContext {
-    isPokemonsLoaded: boolean;
-    arrayOfPokemons: [] | undefined;
-    theresErrorWhileGettingData: boolean;
-    getPokemonById: (id: number) => void;
-    pokemonSearchedById: {} | undefined
+interface IPokemonProps {
+    "100% CP @ 39": number;
+    "100% CP @ 40": number;
+    ATK: number;
+    Aquireable: 1 | 0;
+    "Cross Gen": 1 | 0;
+    DEF: number;
+    "Evolution Stage": number;
+    Evolved: 0 | 1;
+    FamilyId: number;
+    "Future Evolve": 0 | 1;
+    Generation: number;
+    Hatchable: number;
+    "Img name": number;
+    Legendary: 0 | 1;
+    Name: string;
+    Nest: 0 | 1;
+    New: 1 | 0;
+    "Not-Gettable": 0 | 1;
+    "Pokedex Number": number;
+    Raidable: 0 | 1;
+    Regional: 0 | 1;
+    Row: number;
+    STA: number;
+    "STA TOTAL": number;
+    Shiny: number;
+    Spawns: 0 | 1;
+    "Type 1": string;
+    "Weather 1": string;
 }
 
-interface IPokemonProvideProps{
+interface IPokemonApiContext {
+    isPokemonsLoaded: boolean;
+    arrayOfPokemons: IPokemonProps[] | undefined;
+    theresErrorWhileGettingData: boolean;
+    pokemonSearchedById: {} | undefined
+    getPokemonById: (id: number) => void;
+    searchPokemon: (value: string, filter: string) => IPokemonProps[];
+
+}
+
+interface IPokemonProvideProps {
     children: ReactNode;
 }
+
 
 const PokemonApiContext = createContext({} as IPokemonApiContext);
 
 
 
 function PokemonApiProvider({ children }: IPokemonProvideProps) {
-    const [ theresErrorWhileGettingData, setTheresErrorWhileGettingData ] = useState<boolean>(false);
-    const [ isPokemonsLoaded, setIsPokemonsLoaded ] = useState<boolean>(false);
-    const [ arrayOfPokemons, setArrayOfPokemons ] = useState<[]>();
-    const [ pokemonSearchedById, setPokemonSearchedById ] = useState<{}>();
+    const [theresErrorWhileGettingData, setTheresErrorWhileGettingData] = useState<boolean>(false);
+    const [isPokemonsLoaded, setIsPokemonsLoaded] = useState<boolean>(false);
+    const [arrayOfPokemons, setArrayOfPokemons] = useState<IPokemonProps[]>();
+    const [pokemonSearchedById, setPokemonSearchedById] = useState<{}>();
 
 
     useEffect(() => {
         getData();
     }, []);
 
-    async function getData(){
 
-        await axios.get('http://localhost:3030/getPokemons').then( resp => { 
+    async function getData() {
+
+        await axios.get('http://localhost:3030/getPokemons').then(resp => {
             setIsPokemonsLoaded(true);
             setArrayOfPokemons(resp.data);
-        }).catch( err => {
+        }).catch(err => {
             setTheresErrorWhileGettingData(true);
         });
     }
 
-    
-    async function getPokemonById(id: number){
-        await axios.get(`http://localhost:3030/getPokemon/${id}`).then( resp => { 
+
+    async function getPokemonById(id: number) {
+        await axios.get(`http://localhost:3030/getPokemon/${id}`).then(resp => {
             setPokemonSearchedById(resp.data);
         });
     }
 
-    
+    function searchPokemon(value: string, filter: string) {
+        const pokemonsFounded:IPokemonProps[] = [];
+
+        arrayOfPokemons?.forEach((pokemon: any)=> {
+            const filterValue = pokemon[filter].toString().toLowerCase();
+            const valueToSearch = value.toLowerCase();
+
+            if(filterValue?.includes(valueToSearch)) {
+                pokemonsFounded.push(pokemon)
+            }
+
+        });
+
+        return pokemonsFounded;
+    }
+
+
     return (
         <PokemonApiContext.Provider value={{
             isPokemonsLoaded,
             arrayOfPokemons,
             theresErrorWhileGettingData,
             pokemonSearchedById,
-            getPokemonById
+            getPokemonById,
+            searchPokemon
         }}>
 
             {children}
-            
+
         </PokemonApiContext.Provider>
     )
 }
 
-export { 
+export {
     PokemonApiContext,
     PokemonApiProvider
 }
