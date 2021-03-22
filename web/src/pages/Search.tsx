604c0aa8d022921432538957bbf33c303b9206df
Your Name <you@example.com>
2021-03-22 03:28:48 -0300

    Adicionando area de pesquisa.

--- web/src/pages/Search.tsx
+++ web/src/pages/Search.tsx
@@ -1,9 +1,50 @@
-import { Container } from "../styles/pages/search";
+import { Container, SearchAreaContainer } from "../styles/pages/search";
 
-function Search(){
-    return( 
+const selectValues = [
+    "100% CP @ 39",
+    "100% CP @ 40",
+    "ATK",
+    "Aquireable",
+    "Cross Gen",
+    "DEF",
+    "Evolution Stage",
+    "Evolved",
+    "FamilyID",
+    "Future Evolve",
+    "Generation",
+    "Hatchable",
+    "Img name",
+    "Legendary",
+    "Name",
+    "Nest",
+    'New',
+    "Not-Gettable",
+    "Pokedex Number",
+    "Raidable",
+    "Regional",
+    "Row",
+    'STA',
+    "STAT TOTAL",
+    "Shiny",
+    "Spawns",
+    "Type 1",
+    "Type 2",
+    "Weather 1",
+    "Weather 2"
+]
+
+function Search() {
+    return (
         <Container>
-
+            <h1> Pesquisar</h1>
+            <SearchAreaContainer>
+                <input type="text" placeholder="pesquise..."/>
+                <select>
+                    {selectValues.map( value => (
+                        <option value={value}> {value} </option>
+                    ))}
+                </select>
+            </SearchAreaContainer>
         </Container>
     );
 }
--- web/src/styles/pages/search.ts
+++ web/src/styles/pages/search.ts
@@ -1,9 +1,57 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    
+    > h1 {
+        font-family: "Inter";
+        text-align: center;
+        margin: 3vh; 
+        color: #ffffff
+    }
 `;
 
+const SearchAreaContainer = styled.div`
+    height: 60px;
+    width: 90vw;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: flex-end;
+
+    > input { 
+        width: 100%;
+        height: 35px;
+        border-radius: 5px;
+        border: none;
+        background: var(--light-blue);
+        transition: background .7s, color .5s;
+        outline: none;
+        text-align: center;
+        font-size: 1.3rem;
+        color: var(--lighter-blue);
+
+        &:focus { 
+            background: var(--lighter-blue);
+            color: var(--light-blue);
+
+        }
+        
+
+    }
+
+    > select {
+        width: 55%;
+        height: 30px;
+        border-radius: 8px;
+        background: var(--dark-blue);
+        color: #ffffff;
+        outline: none;
+        padding-left: 10%
+    }
+
+`
 export { 
-    Container
+    Container,
+    SearchAreaContainer
 }
