054d2f94561bddd307a20b385d54d88f63ff8470
Your Name <you@example.com>
2021-03-22 20:39:17 -0300

    Adicionando página de detalhes do pokémon.

---
+++ web/src/pages/PokemonData.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../styles/pages/pokemonData"
+
+function PokemonData(){ 
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default PokemonData
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,12 +1,14 @@
 import { Switch, Route, Redirect} from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import PokemonData from "./pages/Search";
 
 function Routes() {
     return (
         <Switch> 
             <Route path="/" exact component={Home}/>
             <Route path="/search" component={Search}/>
+            <Route path="/pokemonData/:id" component={PokemonData}/>
             <Redirect from="*" to="/"/>
         </Switch>
     )
