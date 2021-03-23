0e5ce33550ac0dc8b8a169c2dfbb95be467c27f2
Your Name <you@example.com>
2021-03-23 04:49:58 -0300

    Adicionando lista de até 36 pokemons por página na lista de pokemons.

--- web/src/pages/ListOfPokemons.tsx
+++ web/src/pages/ListOfPokemons.tsx
@@ -1,9 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import PokemonItem from "../components/pokemonItem";
+import { PokemonApiContext } from "../contexts/pokemonApi";
+import LoadingAnimation from "../images/loadingAnimation.json"
 import { Container } from "../styles/pages/listOfPokemons";
 
-function ListOfPokemons() {
+interface IListOfPokemons {
+    history: {
+        push: (path: string) => void;
+    }
+}
+
+function ListOfPokemons(props: IListOfPokemons) {
+    const { arrayOfPokemons } = useContext(PokemonApiContext);
+    const [minPositionAtArray, setMinPositionAtArray] = useState<number>(0);
+    const [currentPokemons, setCurrentPokemons] = useState<[]>();
+
+    useEffect(getElements, [minPositionAtArray, arrayOfPokemons])
+
+
+    function getElements() {
+        const listToShow = arrayOfPokemons?.filter((pokemon, index) => {
+            return index >= minPositionAtArray && index <= minPositionAtArray + 35;
+        });
+
+        setCurrentPokemons(listToShow as any)
+    }
+
     return (
         <Container>
+            {currentPokemons && <h1> Lista de Pokemons </h1>}
             
+            
+            {currentPokemons && <p> {minPositionAtArray + 36} de {arrayOfPokemons?.length}</p> }
+
+            
+
+            {
+                currentPokemons ?
+                    currentPokemons.map((pokemonObj: any) => <PokemonItem pokemonObj={pokemonObj} push={props.history.push} key={pokemonObj.Row} />)
+                    :
+                    <Lottie options={{
+                        animationData: LoadingAnimation
+                    }} />
+            }
         </Container>
     )
 }
--- web/src/styles/pages/listOfPokemons.ts
+++ web/src/styles/pages/listOfPokemons.ts
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+    height: 100%;
+    padding: 10px;
 
+    > h1 { 
+        color: var(--white);
+        text-align: center;
+        font-family: "Inter";
+    }
+
+    > p { 
+        color: var(--white);
+        font-family: "Nunito";
+        margin: 20px;
+        text-align: center;
+    }
 `
 
 export {
