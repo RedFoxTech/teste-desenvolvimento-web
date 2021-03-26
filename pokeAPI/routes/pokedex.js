a867e2251a084c8c82b631b1577c57bbc97f3307
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-26 08:54:10 -0300

    editado API

--- pokeAPI/routes/pokedex.js
+++ pokeAPI/routes/pokedex.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+const db = 'mongodb+srv://userdb:senhadb@cluster0.f39tr.mongodb.net/angularpokedex?retryWrites=true&w=majority'
+
+mongoose.connect(db,err =>{
+  if(err){
+    console.log("\n Erro! \n"+err);
+  }else{
+    console.log("Connected to MongoDB!");
+  }
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
