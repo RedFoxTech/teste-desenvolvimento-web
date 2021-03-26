67adf59a3c3c51e9960ffa00469e4b1a68dd5e7a
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-26 09:50:02 -0300

    criado rota para pegar dados de pokemon

--- pokeAPI/routes/pokedex.js
+++ pokeAPI/routes/pokedex.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Pokemon = require('../models/pokemon')
 var mongoose = require('mongoose');
 const db = 'mongodb+srv://userdb:senhadb@cluster0.f39tr.mongodb.net/angularpokedex?retryWrites=true&w=majority'
 
@@ -12,8 +13,14 @@ mongoose.connect(db,err =>{
 })
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/pokemon/getall', function(req, res, next) {
+  Pokemon.find({},(err,data)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.send(data);
+    }
+  });
 });
 
 module.exports = router;
