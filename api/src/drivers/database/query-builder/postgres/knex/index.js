ed0689e4be57f5b112fb9de2b54f806f21efa576
edmilson-dk <droidvidaboa@gmail.com>
2021-03-26 22:51:31 -0300

    feat: added knex database config

---
+++ api/src/drivers/database/query-builder/postgres/knex/index.js
@@ -0,0 +1,6 @@
+const environment = process.env.NODE_END || 'development';
+
+const knexfile = require('../../../../../../knexfile')[environment];
+const db = require('knex')(knexfile);
+
+module.exports = { db };
