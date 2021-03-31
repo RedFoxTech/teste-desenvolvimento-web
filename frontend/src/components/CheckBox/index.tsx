cd9b1ac9908583aa5cc26baca88853144e98f25c
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-03-31 12:43:49 -0300

    Add CheckBox Component

---
+++ frontend/src/components/CheckBox/index.tsx
@@ -0,0 +1,51 @@
+import { InputHTMLAttributes, useState } from 'react'
+import * as S from './styles'
+
+export type CheckboxProps = {
+  onCheck?: (status: boolean) => void
+  isChecked?: boolean
+  label?: string
+  labelFor?: string
+  labelColor: 'white' | 'black'
+  value?: string | ReadonlyArray<string> | number
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Checkbox = ({
+  onCheck,
+  isChecked = false,
+  label,
+  labelFor = '',
+  labelColor = 'white',
+  value,
+  ...props
+}: CheckboxProps) => {
+  // controlled component (state)
+  const [checked, setChecked] = useState(isChecked)
+
+  const onChange = () => {
+    const status = !checked // true => false => true
+    setChecked(status)
+
+    !!onCheck && onCheck(status)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+        value={value}
+        {...props}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Checkbox
