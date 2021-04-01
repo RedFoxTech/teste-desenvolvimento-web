ce946dbf8f5405abb5b603e72b77eb52151e142b
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-04-01 08:59:56 -0300

    Add Documentation with Swagger on route /api-docs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response, Application } from "express";
 import routes from "./routes";
 import mongoose from "mongoose";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+import * as swaggerDoc from "../docs/swagger.json";
 
 const PORT = process.env.PORT || 3333;
 
@@ -13,6 +15,7 @@ mongoose
     app.use(express.json());
     app.use(cors());
     app.use(routes);
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
     app.get("/", (req: Request, res: Response) =>
       res.send("RedFox - Teste Wesley M Oliveira")
