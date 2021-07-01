d3c9c4200dc0491c2f6afca9dcad6ec12741b0a5
jgfn1 <jgfn@cin.ufpe.br>
2021-07-01 11:26:02 -0300

    vuetify installed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Vuetify,
   render: h => h(App)
 }).$mount('#app')
