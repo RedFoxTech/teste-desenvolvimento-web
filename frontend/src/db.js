8092b60a98558b22df94a6655d854b69c12385c4
jgfn1 <jgfn@cin.ufpe.br>
2021-07-04 01:32:29 -0300

    api url updated

--- frontend/src/db.js
+++ frontend/src/db.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import store from "@/store";
 
-const url = "http://[::1]:3000"; // DEV
-
+// const url = "http://[::1]:3000"; // DEV
+const url = "https://pokemon-go-redfox-backend.herokuapp.com/" //PROD
 export default class DB {
   constructor() {
     axios.defaults.headers.common["Authorization"] = store.state.token;
