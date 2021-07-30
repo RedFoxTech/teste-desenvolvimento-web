9dbac284db18ad2f77e17eac0047b188ef79fcc2
Fazer o linting do código ^-^ <lint-action@samuelmeuli.com>
2021-07-30 02:17:55 +0000

    Fix code style issues with ESLint

--- packages/backend/.eslintrc.js
+++ packages/backend/.eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": false,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "google"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  'env': {
+    'browser': false,
+    'es2021': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+  },
 };
