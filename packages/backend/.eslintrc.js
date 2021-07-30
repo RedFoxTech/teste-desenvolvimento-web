46ac1aa65c9c8ad90520486928d543f6b42a6f56
Fazer o linting do código ^-^ <lint-action@samuelmeuli.com>
2021-07-30 03:22:00 +0000

    Fix code style issues with ESLint

--- packages/backend/.eslintrc.js
+++ packages/backend/.eslintrc.js
@@ -1,25 +1,25 @@
 /* global module */
 
 module.exports = {
-    "env": {
-        "browser": false,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "google"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-unused-vars": "warn",
-        // As funções do express começam com uma letra maiúscula
-        "new-cap": 0
-    }
+  'env': {
+    'browser': false,
+    'es2021': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'no-unused-vars': 'warn',
+    // As funções do express começam com uma letra maiúscula
+    'new-cap': 0,
+  },
 };
