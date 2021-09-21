0122e7082c29499e94608aae5c01653cc686ada2
Victor Freitas <victorprogr@gmail.com>
2021-09-21 02:09:06 -0300

    feat: create function to calc staTotal

--- pokemon-api/src/presentation/controllers/add-new-pokemon-controller.ts
+++ pokemon-api/src/presentation/controllers/add-new-pokemon-controller.ts
@@ -8,6 +8,7 @@ import {
   serverError,
 } from 'shared/interfaces';
 import { AddPokemon } from 'domain/usecases/';
+import { calculateStateTotal } from 'shared/utils/calculateStatTotal';
 
 export class AddNewPokemon implements Controller {
   constructor(
@@ -19,8 +20,18 @@ export class AddNewPokemon implements Controller {
     try {
       const { body } = req;
       const error = this.validation.validate(body);
+
       if (error) badRequest(error);
-      const newPokemon = await this.addPokemon.add(body);
+
+      const data = { ...body };
+
+      const { atk, def, sta } = body;
+
+      const statTotal = calculateStateTotal(atk, def, sta);
+      console.log(statTotal);
+      data.statTotal = statTotal;
+
+      const newPokemon = await this.addPokemon.add(data);
 
       return success(newPokemon);
     } catch (error) {
