4b5218b21e40bede7487859c65d5f80772837859
Matheus <matheusaraujo.dev@gmail.com>
2023-04-12 23:44:46 -0300

    Add: Login Page structure

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button, TextField } from '@mui/material';
+import React from 'react';
+import './style.css';
 
 export default function Login() {
   return (
-    <React.Fragment>
-      <h1>Login Page</h1>
-    </React.Fragment>
+    <div>
+      <header className="App">
+        <div className="logo-container"><img src="src/assets/Pokédex_logo.png" /></div>
+        
+        <form>
+          <TextField className="input" label="Nome" variant="standard" />
+          <TextField className="input" label="E-mail" variant="standard" />
+          <TextField className="input" label="Password" variant="standard" type="password" />
+        
+          <div className="buttons-container">
+            <Button className="form-buttons" color='success' variant="contained">LOGIN</Button>
+            <Button className="form-buttons" color='info' variant="contained">CRIAR UMA CONTA</Button>
+          </div>
+        </form>
+      </header>
+    </div>
   )
 }
