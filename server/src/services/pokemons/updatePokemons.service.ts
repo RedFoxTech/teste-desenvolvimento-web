05e9d22a204f1ed52e976f0d8cd033370e8a71a9
kennedybm <kennedy_muniz@hotmail.com>
2023-04-14 09:51:16 -0300

    Fix: foundPokemon and toFoundErrors types, name and pokedexNumber cannot be updated

--- server/src/services/pokemons/updatePokemons.service.ts
+++ server/src/services/pokemons/updatePokemons.service.ts
@@ -3,17 +3,17 @@ import { AppError } from "../../errors/AppError";
 import { IUpdatePokemon } from "../../interfaces/pokemons";
 
 export const updatePokemonService = async (pokemonID: string, data: IUpdatePokemon) => {
-  const foundPokemon = await PokemonsModel.findOne({ _id: pokemonID });
+  const foundPokemon: IUpdatePokemon | null = await PokemonsModel.findOne({ _id: pokemonID });
 
   if (!foundPokemon) {
     throw new AppError(401, "Pokemon not found");
-  } else if (foundPokemon.name === data.name) {
+  } else if (data.name) {
     throw new AppError(400, "Pokemon name cannot be updated");
-  } else if (foundPokemon.pokedexNumber === data.pokedexNumber) {
+  } else if (data.pokedexNumber) {
     throw new AppError(400, "Pokedex number cannot be updated");
   }
 
-  const toFoundErrors: Array<any> = [];
+  const toFoundErrors: Array<IUpdatePokemon> = [];
   toFoundErrors.push(foundPokemon);
 
   toFoundErrors.map((item: any) => {
