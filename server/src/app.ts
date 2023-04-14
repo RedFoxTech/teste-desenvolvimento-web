fdd794a7799a1f21d2dcbae2022e1cd2b12c16df
kennedybm <kennedy_muniz@hotmail.com>
2023-04-14 10:44:40 -0300

    Fix: removed db connection and moved to server.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,8 @@
-import "dotenv/config";
 import express from "express";
 import "express-async-errors";
 import "reflect-metadata";
 import cors from "cors";
-import mongoose from "mongoose";
+
 import handleAppErrorMiddleware from "./middlewares/errors/handleAppError.middleware";
 import pokemonsRoute from "./routers/pokemons/pokemons.routes";
 
@@ -16,8 +15,4 @@ app.use("/pokemons", pokemonsRoute);
 
 app.use(handleAppErrorMiddleware);
 
-mongoose.connect(process.env.MONGO_URL!).then(() => {
-  app.listen(process.env.PORT || 5500, () =>
-    console.log(`listening on port ${process.env.PORT}`)
-  );
-});
+export default app;
