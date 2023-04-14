93f5216ebd784546850540c54ecefd7554601561
kennedybm <kennedy_muniz@hotmail.com>
2023-04-14 02:21:54 -0300

    Feat: added schema validation middleware in update route

--- server/src/routers/pokemons/pokemons.routes.ts
+++ server/src/routers/pokemons/pokemons.routes.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 import {
   createPokemonController,
   listPokemonsController,
+  retrievePokemonController,
+  updatePokemonController,
 } from "../../controllers";
+import { schemaValidation } from "../../middlewares/schema/schemaValidation.middleware";
+import { updatePokemonSchema } from "../../schemas/updatePokemon.schema";
 
 const pokemonsRoute = Router();
 
 pokemonsRoute.get("/", listPokemonsController);
 
 pokemonsRoute.post("/", createPokemonController);
+
+pokemonsRoute.get("/:pokemonID", retrievePokemonController);
+
+pokemonsRoute.patch("/:pokemonID", schemaValidation(updatePokemonSchema), updatePokemonController);
 
 export default pokemonsRoute;
