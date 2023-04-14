3f77458e099fd3d63d18dc5d7e00e461eeafaebd
kennedybm <kennedy_muniz@hotmail.com>
2023-04-14 10:35:15 -0300

    Feat: delete pokemon service added to main services exports

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -2,5 +2,12 @@ import { createPokemonsService } from "./pokemons/createPokemons.service";
 import { listPokemonsService } from "./pokemons/listPokemons.service";
 import { retrievePokemonService } from "./pokemons/retrievePokemons.service";
 import { updatePokemonService } from "./pokemons/updatePokemons.service";
+import { deletePokemonService } from "./pokemons/deletePokemons.service";
 
-export { createPokemonsService, listPokemonsService, retrievePokemonService, updatePokemonService };
+export {
+  createPokemonsService,
+  listPokemonsService,
+  retrievePokemonService,
+  updatePokemonService,
+  deletePokemonService,
+};
