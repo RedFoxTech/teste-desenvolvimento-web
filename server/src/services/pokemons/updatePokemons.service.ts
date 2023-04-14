0cf4c38e6a6240dce68d9124119a964084f2f4cf
kennedybm <kennedy_muniz@hotmail.com>
2023-04-14 02:32:24 -0300

    Feat: route with schema validation, fields cannot be updated woth the same value

---
+++ server/src/services/pokemons/updatePokemons.service.ts
@@ -0,0 +1,30 @@
+import { PokemonsModel } from "../../models/Pokemons";
+import { AppError } from "../../errors/AppError";
+import { IUpdatePokemon } from "../../interfaces/pokemons";
+
+export const updatePokemonService = async (pokemonID: string, data: IUpdatePokemon) => {
+  const foundPokemon = await PokemonsModel.findOne({ _id: pokemonID });
+
+  if (!foundPokemon) {
+    throw new AppError(401, "Pokemon not found");
+  } else if (foundPokemon.name === data.name) {
+    throw new AppError(400, "Pokemon name cannot be updated");
+  } else if (foundPokemon.pokedexNumber === data.pokedexNumber) {
+    throw new AppError(400, "Pokedex number cannot be updated");
+  }
+
+  const pokemonsArr: Array<any> = [];
+  pokemonsArr.push(foundPokemon);
+
+  pokemonsArr.map((item: any) => {
+    for (const [key, value] of Object.entries(data)) {
+      if (item[`${key}`] == `${value}`) {
+        throw new AppError(400, `${key} cannot be updated with the same value`);
+      }
+    }
+  });
+
+  await PokemonsModel.updateOne({ _id: pokemonID }, { $set: { ...data } });
+
+  return true;
+};
