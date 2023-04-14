26ad053aeaf795dc396c759d2ddf98c33be6207d
kennedybm <kennedy_muniz@hotmail.com>
2023-04-14 02:20:14 -0300

    Feat: added name nad pokedexNumber in update interface

--- server/src/interfaces/pokemons/index.ts
+++ server/src/interfaces/pokemons/index.ts
@@ -29,3 +29,35 @@ export interface ICreatePokemon {
   cp40: number;
   cp39: number;
 }
+
+export interface IUpdatePokemon {
+  name?: string;
+  pokedexNumber?: number;
+  imgName?: string;
+  generation?: number;
+  evolutionStage?: string;
+  evolved?: number;
+  familyID?: number;
+  crossGen?: number;
+  type1?: string;
+  type2?: string;
+  weather1?: string;
+  weather2?: string;
+  statTotal?: number;
+  atk?: number;
+  def?: number;
+  sta?: number;
+  legendary?: number;
+  aquireable?: number;
+  spawns?: number;
+  regional?: number;
+  raidable?: number;
+  hatchable?: number;
+  shiny?: number;
+  nest?: number;
+  new?: number;
+  notGettable?: number;
+  futureEvolve?: number;
+  cp40?: number;
+  cp39?: number;
+}
